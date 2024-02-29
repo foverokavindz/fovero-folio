0edaa2b686de6a1ae062606bd6565d37a721daac
Kavinda Maduranga <kavindamadhuranga74@gmail.com>
2024-03-01 02:23:11 +0530

    Created Initial components

---
+++ src/components/about/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the About component.</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Testimonials from './testimonials/Testimonials';
+import Sidebar from './sidebar/Sidebar';
+import Services from './services/Services';
+import Resume from './resume/Resume';
+import Pricing from './pricing/Pricing';
+import Portfolio from './portfolio/Portfolio';
+import home from './home/Home';
+import Contact from './contact/Contact';
+import Blog from './blog/Blog';
+import About from './about/About';
+
+export {
+  Testimonials,
+  Sidebar,
+  Services,
+  Resume,
+  Pricing,
+  Portfolio,
+  home,
+  Contact,
+  Blog,
+  About,
+};
