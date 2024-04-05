c021aba73d2eef3a11ffbcf0ce0aa1f7ec67e92a
Kavinda Maduranga <kavindamadhuranga74@gmail.com>
2024-04-05 10:28:15 +0530

    Refactor Contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,60 +5,48 @@ import emailjs from '@emailjs/browser';
 import './Contact.css';
 
 const NotificationType = {
-  SUCCESS: "SUCCESS",
-  ERROR: "ERROR",
-
+  SUCCESS: 'SUCCESS',
+  ERROR: 'ERROR',
 };
 
 const Contact = () => {
-  const [isSending,setIsSending] = useState(false)
+  const [isSending, setIsSending] = useState(false);
   const form = useRef();
 
- 
-
   const notify = (type) => {
-   
     if (type === NotificationType.SUCCESS) {
-
-      toast.success("Email was sent Successfully!", {
-        position: "top-center"
-      })
-    }else if (type === NotificationType.ERROR){
-
-      toast.error("Email is not sent!", {
-        position: "top-center"
+      toast.success('Email was sent Successfully!', {
+        position: 'top-center',
+      });
+    } else if (type === NotificationType.ERROR) {
+      toast.error('Email is not sent!', {
+        position: 'top-center',
       });
     }
-
-
-
   };
 
   const sendEmail = (e) => {
     e.preventDefault();
-    setIsSending(!isSending)
-
+    setIsSending(!isSending);
 
     emailjs
       .sendForm('service_iipp5p8', 'template_mw9cj3c', form.current, {
         publicKey: 'V_-J9LWhLL0RJXpEY',
       })
 
-         
       .then(
         () => {
-          setIsSending(false); 
-        notify(NotificationType.SUCCESS); 
-        form.current.reset();
+          setIsSending(false);
+          notify(NotificationType.SUCCESS);
+          form.current.reset();
         },
         (error) => {
           console.log('FAILED...', error.text);
-          setIsSending(false); 
-          notify(NotificationType.ERROR); 
-        },
+          setIsSending(false);
+          notify(NotificationType.ERROR);
+        }
       );
   };
-
 
   return (
     <section className="contact container section" id="contact">
@@ -77,8 +65,9 @@ const Contact = () => {
             <div className="contact__form-div">
               <input
                 type="text"
-                className={`contact__form-input ${ isSending ? "disable_contact__form-input" : null}`}
-        
+                className={`contact__form-input ${
+                  isSending ? 'disable_contact__form-input' : null
+                }`}
                 placeholder="Insert your name"
                 name="user_name"
                 disabled={isSending}
@@ -87,7 +76,9 @@ const Contact = () => {
             <div className="contact__form-div">
               <input
                 type="email"
-                className={`contact__form-input ${ isSending ? "disable_contact__form-input" : null}`}
+                className={`contact__form-input ${
+                  isSending ? 'disable_contact__form-input' : null
+                }`}
                 placeholder="Insert your email"
                 name="user_email"
                 disabled={isSending}
@@ -97,7 +88,9 @@ const Contact = () => {
           <div className="contact__form-div">
             <input
               type="text"
-              className={`contact__form-input ${ isSending ? "disable_contact__form-input" : null}`}
+              className={`contact__form-input ${
+                isSending ? 'disable_contact__form-input' : null
+              }`}
               placeholder="Insert your subject"
               name="user_subject"
               disabled={isSending}
@@ -110,19 +103,26 @@ const Contact = () => {
               id=""
               cols={30}
               rows={10}
-              className={`contact__form-input ${ isSending ? "disable_contact__form-input" : null}`}
+              className={`contact__form-input ${
+                isSending ? 'disable_contact__form-input' : null
+              }`}
               placeholder="Write your message"
               disabled={isSending}
             ></textarea>
           </div>
 
-          <button type="submit" className={`btn ${ isSending ? "disabled-btn" : null}`} value="Send"  disabled={isSending} onClick={() => notify()}>
-            { isSending ? "Sending..." : "Send message"}
-     
+          <button
+            type="submit"
+            className={`btn ${isSending ? 'disabled-btn' : null}`}
+            value="Send"
+            disabled={isSending}
+            onClick={() => notify()}
+          >
+            {isSending ? 'Sending...' : 'Send message'}
           </button>
         </form>
       </div>
-      <ToastContainer autoClose={3000}/>
+      <ToastContainer autoClose={3000} />
     </section>
   );
 };
