4ce21df134fab101577930169d57f6903ea9bdc1
Kavinda Maduranga <kavindamadhuranga74@gmail.com>
2025-12-18 21:55:14 +0530

    Update about me

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,33 @@
-import {
-  Testimonials,
-  Sidebar,
-  Services,
-  Resume,
-  Pricing,
-  Portfolio,
-  Home,
-  Contact,
-  Blog,
-  About,
-  TechStack,
-} from './components';
+import { Testimonials, Sidebar, Services, Resume, Pricing, Portfolio, Home, Contact, Blog, About, TechStack } from './components';
 import './App.css';
 import ThemeToggle from './components/themeToggle/ThemeToggle';
+import ClickSpark from './animations/ClickSpark';
+import { useTheme } from './context/theme';
 
 function App() {
-  return (
-    <>
-      <ThemeToggle />
-      <Sidebar />
-      <main className="main">
-        <Home />
-        <About />
-        <Services />
-        <TechStack />
-        <Resume />
-        <Portfolio />
-        <Pricing />
-        <Blog />
-        <Testimonials />
-        <Contact />
-      </main>
-    </>
-  );
+	const { isDarkMode } = useTheme();
+	return (
+		<>
+			<ThemeToggle />
+			<Sidebar />
+			<main className="main">
+				<ClickSpark sparkColor={isDarkMode ? '#FF4757' : '#1A1A2E'} sparkSize={20} sparkRadius={70} sparkCount={10} duration={800} extraScale={1.2}>
+					<>
+						<Home />
+						<About />
+						<Services />
+						<TechStack />
+						<Resume />
+						<Portfolio />
+						<Pricing />
+						<Blog />
+						<Testimonials />
+						<Contact />
+					</>
+				</ClickSpark>
+			</main>
+		</>
+	);
 }
 
 export default App;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,28 +4,30 @@ import Me from '../../assets/myface.png';
 import HeaderSocials from './HeaderSocials';
 import ScrollDown from './ScrollDown';
 import Shapes from './Shapes';
+import TextType from '../../animations/textType/TextType';
 
 const Home = () => {
-  return (
-    <section className="home container" id="home">
-      <div className="intro">
-        <div className="home__img-wrapper">
-          <img src={Me} alt="" className="home__img" />
-          <div className="home__img-decoration">
-   
-          </div>
-        </div>
-        <h1 className="home__name">Kavinda Weerasinghe</h1>
-        <span className="home__education">Full-Stack Developer & UI Designer</span>
-        <HeaderSocials />
-        <a href="#contact" className="btn">
-          Let's Talk
-        </a>
-      </div>
-      <ScrollDown />
-      <Shapes />
-    </section>
-  );
+	return (
+		<section className="home container" id="home">
+			<div className="intro">
+				<div className="home__img-wrapper">
+					<img src={Me} alt="" className="home__img" />
+					<div className="home__img-decoration"></div>
+				</div>
+				<h1 className="home__name">Kavinda Weerasinghe</h1>
+				<span className="home__education">Full-Stack Engineer & Designer</span>
+				<div className="memphis-bg-texttype">
+					<TextType text={['Crafting Code & Shaping Solution', 'Code Artist + Problem Solver', 'Simplifying Complexity + Enhancing Experience']} typingSpeed={75} pauseDuration={2000} showCursor={true} cursorCharacter="●" />
+				</div>
+				<HeaderSocials />
+				<a href="#contact" className="btn">
+					Let's Talk
+				</a>
+			</div>
+			<ScrollDown />
+			<Shapes />
+		</section>
+	);
 };
 
 export default Home;
